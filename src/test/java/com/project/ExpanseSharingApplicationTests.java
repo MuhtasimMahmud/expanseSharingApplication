032c220818e02f34fc646dc4b4f481314398042f
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-09-18 16:42:34 +0600

    Initial commit

---
+++ src/test/java/com/project/ExpanseSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpanseSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
