34c16fa3760f3a7baa2e14af4e84365bd0a74a71
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-12-12 21:19:23 +0600

    updated

---
+++ src/main/java/com/project/Controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.project.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("expanceSharingApplication/home")
+    public String home(){
+        return "home";
+    }
+
+
+}
